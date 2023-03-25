c99d4538a97df303b53962ab9ded3258b533639b
Chris Rempel <chrisrempel@exosite.com>
2023-03-24 17:54:26 -0700

    Add dialyzer.

--- mix.exs
+++ mix.exs
@@ -6,6 +6,7 @@ defmodule LibclusterDB.Mixfile do
       app: :libcluster_db,
       version: "0.6.0",
       elixir: "~> 1.4",
+      dialyzer: [plt_add_deps: :app_tree],
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -19,6 +20,7 @@ defmodule LibclusterDB.Mixfile do
   defp deps do
     [
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
       {:libcluster, "~> 3.0"},
       {:mongodb_driver, "~> 1.0"}
     ]
